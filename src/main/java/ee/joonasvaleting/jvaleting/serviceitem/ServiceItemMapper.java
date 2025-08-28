29d7b42da954a76d68b521c979ac095f980aabe4
Aleksandr Kaidalov <kaidalovaleksandr@gmail.com>
2025-08-28 23:57:46 +0300

    fix: delete unnecessary @Component annotation

--- src/main/java/ee/joonasvaleting/jvaleting/serviceitem/ServiceItemMapper.java
+++ src/main/java/ee/joonasvaleting/jvaleting/serviceitem/ServiceItemMapper.java
@@ -1,9 +1,7 @@
 package ee.joonasvaleting.jvaleting.serviceitem;
 
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
 
-@Component
 @Mapper(componentModel = "spring")
 public interface ServiceItemMapper {
     ServiceItemDto fromDbToDto(ServiceItem serviceItem);
